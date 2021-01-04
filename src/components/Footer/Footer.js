9168722ea20a446a8b54d5efaea62bdeab74ad2f
Shijing Liu <brendaliu0628@gmail.com>
2021-01-04 10:24:55 -0600

    complete footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,47 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+
+
 function Footer() {
  return (
   <div className="footer">
-   footer
+   <div className="footer__left">
+    <img className="footer__albumLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Record-Album-02.jpg/220px-Record-Album-02.jpg" alt="" />
+    <div className="footer__songInfo">
+     <h4>No song is playing</h4>
+     <p>...</p>
+    </div>
+   </div>
+
+   <div className="footer__center">
+    <ShuffleIcon className="footer__green" />
+    <SkipPreviousIcon className="footer__icon" />
+    <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+    <SkipNextIcon className="footer__icon" />
+    <RepeatIcon className="footer__green" />
+   </div>
+
+   <div className="footer__right">
+    <Grid container spacing={2}>
+     <Grid item>
+      <PlaylistPlayIcon />
+     </Grid>
+     <Grid item>
+      <VolumeDownIcon />
+     </Grid>
+     <Grid item xs>
+      <Slider />
+     </Grid>
+    </Grid>
+   </div>
   </div>
  )
 }
