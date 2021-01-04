a7d7b4334b11c73c630c86aa7531a1cd86870190
Shijing Liu <brendaliu0628@gmail.com>
2021-01-03 21:15:01 -0600

    sidebar fetch playlist from spotify once I login

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarOptions from './SidebarOptions'
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import { useDataLayerValue } from '../../DataLayer';
+
+
 function Sidebar() {
+ const [{ playlists }, dispatch] = useDataLayerValue()
+
  return (
   <div className="sidebar">
-   <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo" />
+   <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo" />
+   <SidebarOptions Icon={HomeIcon} title="Home" />
+   <SidebarOptions Icon={SearchIcon} title="Search" />
+   <SidebarOptions Icon={LibraryMusicIcon} title="Your Library" />
+   <br></br>
+   <strong className="sidebar__title">PLAYLISTS</strong>
+   <hr></hr>
+   { playlists?.items?.map((playlist, i) => (
+    <SidebarOptions key={i} title={playlist.name} />
+   ))}
   </div>
  )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,29 +1,36 @@
 export const initialState = {
- user: null,
- playlist: [],
- playing: false,
- item: null,
- token: 'BQAFT0hbeb9mtl08AjA4WqiZH2X3osI9PlKqddvtZ8ZdRqfJzrxLt3EifTYSI3T27mxBANvOaWVdCQfj9wXBCKcHo-PAtjd8Cy8nJOJi-0CyDM6DKB4q6LEbLfwyOk3kaotPPSHK58Pzhlm0I0YBPzrW3N1-dr9efvSJDZ9cYv2xltpZ'
+  user: null,
+  playlist: [],
+  playing: false,
+  item: null,
+  token: 'BQAFT0hbeb9mtl08AjA4WqiZH2X3osI9PlKqddvtZ8ZdRqfJzrxLt3EifTYSI3T27mxBANvOaWVdCQfj9wXBCKcHo-PAtjd8Cy8nJOJi-0CyDM6DKB4q6LEbLfwyOk3kaotPPSHK58Pzhlm0I0YBPzrW3N1-dr9efvSJDZ9cYv2xltpZ'
 }
 
 const reducer = (state, action) => {
- console.log(action)
+  console.log(action)
 
- // eslint-disable-next-line default-case
- switch (action.type) {
-  case 'SET_USER':
-   return {
-    ...state,
-    user: action.user
-   };
-  case 'SET_TOKEN':
-   return {
-    ...state,
-    token: action.token
-   }
-  default:
-   return state;
- }
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user
+      };
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token
+      }
+    case 'SET_PLAYLISTS':
+      return {
+        ...state,
+        playlists: action.playlists
+      }
+
+
+    default:
+      return state;
+  }
 }
 
 export default reducer
